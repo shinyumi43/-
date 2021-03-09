ba5d8ca53995b5ba0b0d3f351e00a31df5bae8fa
Shin Yu Mi <79843233+shinyumi43@users.noreply.github.com>
2021-03-09 19:39:41 +0900

    Add files via upload

---
+++ array(2).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Array {
+private:
+	int* arr;
+	int arrSize;
+public:
+	Array(int size){
+		this->arrSize = size;
+		this->arr = new int[arrSize];
+		for (int i = 0; i < arrSize; i++) {
+			arr[i] = 0;
+		}
+	}
+
+	void Insert() {
+		for (int i = 0; i < arrSize; i++) {
+			cin >> arr[i];
+		}
+	}
+
+	void Print() {
+		for (int i = 0; i < arrSize; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+	void Shift() {
+		int sf = arr[arrSize - 1];
+		for (int k = arrSize - 2; k >= 0; k--) {
+			arr[k + 1] = arr[k];
+		}
+		arr[0] = sf;
+	}
+
+};
+
+int main() {
+	int T, N, D;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> N >> D;
+		Array ar(N);
+		ar.Insert();
+		for (int j = 0; j < D; j++) {
+			ar.Shift();
+		}
+		ar.Print();
+	}
+	return 0;
+}
